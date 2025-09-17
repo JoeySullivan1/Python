e3cdfd27ddd13e1b459d484b39f7431302aa55a4
JoeySullivan1 <pollo2reliptube1@gmail.com>
2025-09-17 17:05:05 -0600

    Mi primer Commit

---
+++ calc.py
@@ -0,0 +1,8 @@
+def suma(a, b):
+    try:
+ # Convertir a float para ver si el dato es y un numero valido
+        a = float(a)
+        b = float(b)
+        return a + b
+    except ValueError:
+        return "error"
