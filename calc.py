b5a0d17e37486aeebe45f099161d77a393c878bc
JoeySullivan1 <pollo2reliptube1@gmail.com>
2025-10-15 11:51:14 -0600

    Prueba

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 def suma(a, b):
     try:
- # Convertir a float para ver si el dato es y un numero valido
+ # Convertir a float para ver si el dato es y un numero valido!
         a = float(a)
         b = float(b)
         return a + b
